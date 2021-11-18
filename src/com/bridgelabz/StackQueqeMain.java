4d3df426cf332bc853bb394d3db9c50332ea8aa8
Textspot <sachinkatarki1995@gmail.com>
2021-11-18 13:40:07 +0530

    Added:Creat dequeue from the begining Using Linked List

--- src/com/bridgelabz/StackQueqeMain.java
+++ src/com/bridgelabz/StackQueqeMain.java
@@ -9,6 +9,7 @@ public class StackQueqeMain {
 	        System.out.println("Press '2' to peek the element");
 	        System.out.println("Press '3' to pop the element");
 	        System.out.println("Press '4' to add element in the Queqe");
+	        System.out.println("Press '5' to dequeue element");
 	        switch (sc.nextInt()) {
 	            case 1:
 	                Operation.addElement();
@@ -23,14 +24,16 @@ public class StackQueqeMain {
 	            case 4:
 	                Operation.queueElement();
 	                break;
+	            case 5:
+	                Operation.dequeueElement();
+	                break;
 	            default:
 	            {
 	                System.out.println("Press Valid Key");
 	            }
 	        }
+	    }
 	}
-}
-		
 
 	
 
